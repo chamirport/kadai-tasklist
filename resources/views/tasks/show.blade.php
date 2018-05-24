ef7c6c0814af82428c92cd6e992ea49ca8805d32
EC2 Default User <ec2-user@ip-172-31-47-111.us-east-2.compute.internal>
2018-05-24 02:09:15 +0000

    bootatrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -7,10 +7,10 @@
     <p>タスク: {{ $task->content }}</p> 
     <p>ステータス: {{ $task->status }}</p>
 
-    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
     
 @endsection
